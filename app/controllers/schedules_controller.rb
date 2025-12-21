344341ed8018ea2763bf5ef52f3c3d2b9e61b742
kaamrn04 <kaamrn.0414@gmail.com>
2025-12-21 14:18:22 +0900

    Scheduleプリの実装

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,33 @@
+class SchedulesController < ApplicationController
+
+def home
+ @schedules = Schedule.all
+end
+
+#一覧
+def index
+end
+
+#詳細
+def show
+end
+
+
+#新規
+def new
+end
+
+#編集
+def edit
+end
+
+#削除
+def destroy
+end
+
+
+#更新
+def update
+end
+
+end
